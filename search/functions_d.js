a4e25674a1531e6d96ab6533c8e8563534545572
hanslhansl <46355861+hanslhansl@users.noreply.github.com>
2025-06-09 20:06:47 +0000

    Deploying to gh-pages from @ hanslhansl/cpptkinter@6aee66e99bf1e6fedc6ac6fc99b6fa5783a28c3f 🚀

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['nametowidget_0',['nametowidget',['../classcpptkinter_1_1Misc.html#a4fb0c53d8ae5fafb298d3193487bd11f',1,'cpptkinter::Misc::nametowidget(std::string_view name)'],['../classcpptkinter_1_1Misc.html#aa8d9aa02745a9b075f6c6b97f029fd6f',1,'cpptkinter::Misc::nametowidget(const _cpptkinter::tk_window_type &amp;window)']]],
+  ['nametowidget_0',['nametowidget',['../classcpptkinter_1_1Misc.html#a53c82395921f25990cac6ca00616ef82',1,'cpptkinter::Misc::nametowidget(std::string_view name) const'],['../classcpptkinter_1_1Misc.html#a42dec24e3fe8faa828da9ab2aedb7823',1,'cpptkinter::Misc::nametowidget(const _cpptkinter::tk_window_type &amp;window) const']]],
   ['nearest_1',['nearest',['../structcpptkinter_1_1Listbox.html#a5d83cde2f73c695ceb8e14e8e34f316c',1,'cpptkinter::Listbox']]],
   ['new_5fpythoncmd_5fclientdata_2',['new_PythonCmd_ClientData',['../namespacecpptkinter_1_1detail.html#a5bc3353468986595b19297bd9b889ec0',1,'cpptkinter::detail']]],
   ['next_3',['next',['../structcpptkinter_1_1ttk_1_1Treeview.html#aeb6212e53bc362cae9113250bbf9a555',1,'cpptkinter::ttk::Treeview']]],
